01c2ba2999a217e2eab6dec37179b5745e0f25bf
aya alaa <aya-28-10@hotmail.com>
2025-09-04 12:06:29 +0300

    update loading

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className='flex justify-center items-center h-20'>
-      <div className='w-10 h-10 border-4 border-black dark:border-white rounded-full border-t-transparent animate-spin'></div>
+      <div className='w-10 h-10 border-4 border-black dark:border-white rounded-full border-t-transparent dark:border-t-black animate-spin'></div>
     </div>
   );
 };
